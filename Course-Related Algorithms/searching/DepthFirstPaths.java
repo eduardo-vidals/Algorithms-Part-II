64a3116c5a13899ec105236df550bd9fc6bd5b8c
Eduardo Vidals <63515838+eduardovidals@users.noreply.github.com>
2021-03-15 00:36:31 -0400

    Algorithms Part II

---
+++ Course-Related Algorithms/searching/DepthFirstPaths.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computerscience.algorithms.search;
+
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.Stack;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class DepthFirstPaths {
+    private final boolean[] marked;
+    private final int[] edgeTo;
+    private final int s;
+
+    public DepthFirstPaths(Graph G, int s) {
+        this.s = s;
+        edgeTo = new int[G.V()];
+        marked = new boolean[G.V()];
+        validateVertex(s);
+        dfs(G, s);
+    }
+
+    private void dfs(Graph G, int v) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        validateVertex(v);
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        validateVertex(v);
+        if (!hasPathTo(v)) {
+            return null;
+        }
+        Stack<Integer> path = new Stack<>();
+        for (int x = v; x != s; x = edgeTo[x]) {
+            path.push(x);
+        }
+        path.push(s);
+        return path;
+    }
+
+    private void validateVertex(int v) {
+        int V = marked.length;
+        if (v < 0 || v >= V) {
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph G = new Graph(7);
+        G.addEdge(1, 0);
+        G.addEdge(2, 1);
+        G.addEdge(4, 0);
+        G.addEdge(5, 0);
+        G.addEdge(6, 3);
+        System.out.println(G);
+        DepthFirstPaths dfs = new DepthFirstPaths(G, 2);
+        System.out.println(dfs.pathTo(6));
+    }
+
+}
